9b4afa2cc57b4abe46c30795a4374470c36ba41e
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2024-10-20 03:11:45 +0100

    Developper le boutton Book a table par java script

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+const showReservationFormBtn = document.getElementById('showReservationFormBtn'); // Bouton existant dans la navbar
+const reservationFormContainer = document.getElementById('reservationFormContainer');
+const closeReservationFormBtn = document.getElementById('closeReservationFormBtn');
+
+showReservationFormBtn.addEventListener('click', function() {
+    reservationFormContainer.style.display = 'flex';
+});
+
+closeReservationFormBtn.addEventListener('click', function() {
+    reservationFormContainer.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == reservationFormContainer) {
+        reservationFormContainer.style.display = 'none';
+    }
+});
+
+// Gérer le clic sur le bouton Annuler
+cancelReservationBtn.addEventListener('click', function() {
+    reservationFormContainer.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == reservationFormContainer) {
+        reservationFormContainer.style.display = 'none';
+    }
+});
